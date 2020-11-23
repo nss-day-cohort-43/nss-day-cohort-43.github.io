a26696eb04d7f4c38d69e141120a3c1614d7bb61
Aaron Gertler <aaron.gertler@gmail.com>
2020-11-23 14:02:04 -0600

    added portfolio section

--- src/App.js
+++ src/App.js
@@ -38,6 +38,55 @@ function App() {
           <p className="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
         </div>
       </header>
+
+      <section className="page-section portfolio" id="portfolio">
+        <div className="container">
+
+          {/* Portfolio Section Heading */}
+          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+
+          {/* Icon Divider */}
+          <div className="divider-custom">
+            <div className="divider-custom-line"></div>
+            <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+            <div className="divider-custom-line"></div>
+          </div>
+
+          {/* Portfolio Grid Items */}
+          <div className="row justify-content-center">
+
+            {/* Portfolio Item 1 */}
+            <div className="col-md-6 col-lg-4 mb-5">
+              <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                  <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                </div>
+                <img className="img-fluid" src={logo} alt="" />
+              </div>
+            </div>
+
+            {/* Portfolio Item 2 */}
+            <div className="col-md-6 col-lg-4 mb-5">
+              <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
+                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                  <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                </div>
+                <img className="img-fluid" src={logo} alt="" />
+              </div>
+            </div>
+
+            {/* Portfolio Item 3 */}
+            <div className="col-md-6 col-lg-4 mb-5">
+              <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal3">
+                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                  <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                </div>
+                <img className="img-fluid" src={logo} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
