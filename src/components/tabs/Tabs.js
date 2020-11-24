065f4a8d6b9c25fc9f5eb2c96e7978b663ee0469
hrissolo <hrissolo@gmail.com>
2020-11-24 14:54:00 -0500

    tabs component

---
+++ src/components/tabs/Tabs.js
@@ -0,0 +1,13 @@
+import Tabs from 'Bootstrap'
+
+<Tabs defaultActiveKey="everyone" id="uncontrolled-tab-example">
+  <Tab eventKey="everyone" title="Everyone">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="uiUx" title="UI/UX">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="fullstack" title="Full Stack">
+    <Sonnet />
+  </Tab>
+</Tabs>
