cb5b6462fd0f78e2d7a276c2625c6e09dd150d7b
bcdavis <bcollinsdavis@gmail.com>
2020-11-24 09:22:59 -0600

    Added basic card skeleton with placeholder for image and button icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from '../node_modules/startbootstrap-freelancer/src/assets/img/avataaars.svg'
 import './App.scss'
+import { CardHTML } from "./components/cards/Card.js"
+import React from "react";
 
 function App() {
   return (
@@ -57,12 +59,7 @@ function App() {
 
             {/* Portfolio Item 1 */}
             <div className="col-md-6 col-lg-4 mb-5">
-              <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-                <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                  <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                </div>
-                <img className="img-fluid" src={logo} alt="" />
-              </div>
+              <CardHTML />
             </div>
 
             {/* Portfolio Item 2 */}
---
+++ src/components/cards/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Card, Button, ButtonGroup, Image } from 'react-bootstrap';
+import './customCard.scss';
+
+export const CardHTML = () => {
+
+
+
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                <div className="card-focus-banner--uiux">
+                    <p>UI/UX Front-End Developer</p>
+                </div>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Name Here</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <ButtonGroup className="icons" aria-label="contact-links">
+                        <Button className="btn-link linkedin-link" variant="light">
+                            <Image src="" />
+                        </Button>
+                        <Button className="btn-link github-link" variant="light">
+                            <Image src="" />
+                        </Button>
+                        <Button className="btn-link personalSite-link" variant="light">
+                            <Image src="" />
+                        </Button>
+                        <Button className="btn-link other-link" variant="light">
+                            <Image src="" />
+                        </Button>
+                    </ButtonGroup>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
