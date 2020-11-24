b69d7df13d4a9d2542665c43b95f150034e6f610
Devco2011 <devco2011@gmail.com>
2020-11-24 13:04:38 -0600

    nav with button v1

--- src/components/nav/RSVP.js
+++ src/components/nav/RSVP.js
@@ -7,8 +7,8 @@ export const RSVPButton = () => (
     // Changed "warning" to match our yellow in variables.scss
 
     <div className="text-center mt-4">
-        <div className="rsvpButton">
-            <a className="rsvp btn  btn-warning" href="https://www.eventbrite.com/e/nashville-software-school-info-session-tickets-76478111089">
+        <div className="rsvpButton pl-5">
+            <a className="rsvp btn  btn-warning ml-10 mb-4" href="https://www.eventbrite.com/e/nashville-software-school-info-session-tickets-76478111089">
                 <i className="fas fa-download mr-2"></i>
                 <strong>RSVP FOR DEMO DAY FEB 12, 2021</strong>
             </a>
