08b20e120a684fec027d7bf7998299af6fc4faff
Audrey Thomasson <audrey@macbook-pro.lan>
2020-11-24 15:40:23 -0600

    updated fetch call

--- src/components/card/CardList.js
+++ src/components/card/CardList.js
@@ -1,50 +1,61 @@
 // By Audrey Thomasson
 
 import React, { useEffect, useState } from "react"
-import { Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { CardHTML } from "./Card"
-import { MateContext } from "./MateProvider";
 
 export const CardList = () => {
-    const { mates, getMates, searchTerms } = useContext(MateContext)
+    const [allMates, setAllMates] = useState([])
 
     // Since you are no longer ALWAYS displaying all of the classmates
     const [ filteredMates, setFiltered ] = useState([])
 
-    // Empty dependency array - useEffect only runs after first render
-    useEffect(() => {
-        getMates()
-    }, [])
+	const getAllMates = () => {
+        // return fetch("http://localhost:8088/cohortMates")
+        return fetch ("database.json")
+        	.then(response => response.json())
+	}
+	useEffect(() => {
+		getAllMates()
+			.then(data => {
+				console.log(data)
+				setAllMates(data.cohortMates)
+			})
+	}, [])
 
 
-    // useEffect dependency array with dependencies - will run if dependency changes (state)
-    // Filters for searchTerms and finds the classmate by first OR last name
-    // searchTerms will cause a change
-    useEffect(() => {
-        if (searchTerms !== "") {
-            // If the search field is not blank, display matching shots
-            const subset = mates.filter(mate => mate.firstName.toLowerCase().includes(searchTerms.toLowerCase()) || mate.lastName.toLowerCase().includes(searchTerms.toLowerCase()))
-            setFiltered(subset)
-        } else {
-            // If the search field is blank, display all shots matching the activeUser
-            setFiltered(mates)
-        }
-    }, [searchTerms, mates])
+    // // Filters for searchTerms and finds the classmate by first OR last name
+    // // searchTerms will cause a change
+    // useEffect(() => {
+    //     if (searchTerms !== "") {
+    //         // If the search field is not blank, display matching shots
+    //         const subset = mates.filter(mate => mate.firstName.toLowerCase().includes(searchTerms.toLowerCase()) || mate.lastName.toLowerCase().includes(searchTerms.toLowerCase()))
+    //         setFiltered(subset)
+    //     } else {
+    //         // If the search field is blank, display all shots matching the activeUser
+    //         setFiltered(mates)
+    //     }
+    // }, [searchTerms, mates])
 
  
 
 
     return (
-        <>              
-                        <Card.Group itemsPerRow={4} stackable className='cardHolder'>
+        <>  
+            <Container class="fluid">           
+                <Row>
+                    <Col xs={12} s={12} md={6} l={4} xl={4}>
                         <>
                             {
-                            filteredMates.map(mate => {
-                                return <CardHTML key={mate.id} mate={mate} />
-                            })
+                                allMates.map(mate => {
+                                    return <CardHTML key={mate.id} mate={mate} />
+                                })
                             }
                         </>
-                        </Card.Group>
+                    </Col>
+                </Row>
+
+            </Container> 
 
 
         </>
