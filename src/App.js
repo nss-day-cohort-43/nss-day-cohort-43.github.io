9ea35058270391c40ed349e5f0856adf4d916a31
Colin Arel <romans_one16@live.com>
2020-11-24 10:12:04 -0600

    removed eslintcache again and changed navbar background color to #117785 teal

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
       <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
         <div className="container">
-          <a className="navbar-brand js-scroll-trigger" href="#page-top">C43</a>
+          <a className="navbar-brand js-scroll-trigger" href="#page-top">NSS C43</a>
           <button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
               <i className="fas fa-bars"></i>
