bd5c9533c63288982bcd6aa2a4c09113faf16fad
Aaron Gertler <aaron.gertler@gmail.com>
2020-11-30 14:13:54 -0600

    updating thanks styles

--- src/components/thanks/Thanks.js
+++ src/components/thanks/Thanks.js
@@ -10,8 +10,6 @@ export const Thanks = () => {
 
                 <div className="divider-custom divider-light">
                     <div className="divider-custom-line"></div>
-                    <div className="divider-custom-icon"><i className="fas fa-thumbs-up thanksIcon"></i></div>
-                    <div className="divider-custom-line"></div>
                 </div>
 
                 <div className="row">
