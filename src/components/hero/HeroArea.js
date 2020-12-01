899790cbf3e1d436c515cf6e9ed36e9c5a12bd0c
hrissolo <hrissolo@gmail.com>
2020-12-01 14:21:50 -0600

    hero background progress .. good luck

--- src/components/hero/HeroArea.js
+++ src/components/hero/HeroArea.js
@@ -3,10 +3,14 @@ import Polygons from './Polygons.svg'
 
 export const HeroArea = () => {
   return (
-    <div id="hero" style={{ backgroundImage: `url(${Polygons})` }}>
+    <div id="hero" style={{ backgroundImage: `url(${Polygons})` }}  >
       <h2>Nashville Software School</h2>
           <h1 className="masthead-heading text-uppercase mb-0">Cohort 43</h1>
           
     </div>
   )
 }
+
+
+// background-position="center"
+// style={{height:"690px"}} style={{width:"769px"}}
