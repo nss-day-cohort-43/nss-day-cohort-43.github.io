0cb58ee28a90512cddcca432a5ec806215193232
Colin Arel <romans_one16@live.com>
2020-12-01 16:11:06 -0600

    moved a few nav items around... trying some different layout options. Saving end of day.

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -10,6 +10,7 @@ export const NavBar = (props) => {
         <>
             <Navbar className="nav-container" bg="primary" expand="lg" fixed="top">
                 <Navbar.Brand href="#page-top"><Image className="nssLogo" src="images/placeholder-images/c43-pick_logo.png" fluid /></Navbar.Brand>
+                <RSVPButton />
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
@@ -18,8 +19,8 @@ export const NavBar = (props) => {
                         <Nav.Link href="#tech">Tech</Nav.Link>
                         <Nav.Link href="#thanks">Thanks</Nav.Link>
                     </Nav>
-                    <RSVPButton />
                 </Navbar.Collapse>
+
             </Navbar>
         </>
     )
