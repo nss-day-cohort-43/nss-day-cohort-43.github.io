531cb15428a3c09972eae58ede036e9cf3828c4b
Colin Arel <romans_one16@live.com>
2020-12-02 16:12:06 -0600

    end of day save. header and subheader + cohort 43 logo on page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,8 @@ export const Header = () => {
             <div className="masthead-container">
                 <h1 className="masthead-heading">Hire your next developer</h1>
                 <p className="masthead-subheading">What are you looking for?</p>
-                {/* Masthead Avatar Image */}
-                {/* <img className="masthead-avatar mb-5" src={logo} alt="" /> */}
-                {/* Masthead Heading */}
             </div>
+            <img className="cohort-43-logo" src="../../images/NSS Logo with 43.png" alt="Cohort 43 Logo" />
         </header>
 
     )
