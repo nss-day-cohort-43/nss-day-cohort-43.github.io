90960aa46344e9780b35a47499e334fdf9425311
Colin Arel <romans_one16@live.com>
2020-12-02 15:31:25 -0600

    added header and subheader to Hero area

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,20 +4,13 @@ import './Header.scss'
 export const Header = () => {
     return (
 
-        <header className="masthead bg-white text-white text-center" id="page-top">
-            <div className="container d-flex align-items-center flex-column">
+        <header className="masthead bg-white" id="page-top">
+            <div className="masthead-container">
+                <h1 className="masthead-heading">Hire your next developer</h1>
+                <p className="masthead-subheading">What are you looking for?</p>
                 {/* Masthead Avatar Image */}
                 {/* <img className="masthead-avatar mb-5" src={logo} alt="" /> */}
                 {/* Masthead Heading */}
-                <h1 className="masthead-heading">Hire your next developer</h1>
-                {/* Icon Divider */}
-                {/* <div className="divider-custom divider-light">
-                        <div className="divider-custom-line"></div>
-                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                        <div className="divider-custom-line"></div>
-                    </div> */}
-                {/* Masthead Subheading */}
-                <p className="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
             </div>
         </header>
 
