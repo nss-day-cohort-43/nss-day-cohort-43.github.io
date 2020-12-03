03bde3dc0d015d8081b4d54699c16594715a65a9
Devco2011 <devco2011@gmail.com>
2020-12-03 09:58:39 -0600

    nav bar should be good to go

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { RSVPButton } from './RSVP'
 import { Navbar, Nav } from 'react-bootstrap'
 import './NavBar.scss'
-import Image from 'react-bootstrap/Image'
+
 
 
 export const NavBar = (props) => {
+
+    const [expanded, setExpanded] = useState(false);
+
     return (
         <>
-            <Navbar className="nav-container" bg="primary" expand="lg" fixed="top">
-                <Navbar.Brand href="#page-top"><Image className="nssLogo" src="images/placeholder-images/c43-pick_logo.png" fluid /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mainNavBurger" />
+            <Navbar className="nav-container" bg="primary" expand="lg" fixed="top" expanded={expanded}>
+                <Navbar.Brand href="#page-top"><strong>Nashville Software School</strong></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mainNavBurger" onClick={() => setExpanded(expanded ? false : "expanded")} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#devs">Devs</Nav.Link>
-                        <Nav.Link href="#tech">Tech</Nav.Link>
-                        <Nav.Link href="#thanks">Thanks</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#devs">Developers</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} href="#tech">Our Tech Stack</Nav.Link>
                     </Nav>
                     <RSVPButton />
                 </Navbar.Collapse>
