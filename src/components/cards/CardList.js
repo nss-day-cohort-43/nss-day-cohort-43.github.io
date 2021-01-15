5b4eb703551a866c3922561da8178101fa9f37d0
Aaron Gertler <aaron.gertler@gmail.com>
2021-01-15 15:17:51 -0600

    deleted about, not used

--- src/components/cards/CardList.js
+++ src/components/cards/CardList.js
@@ -1,14 +1,15 @@
 // By Audrey Thomasson
 
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { CardHTML } from "./Card"
 
-export const CardList = ({filteredMates}) => {
+export const CardList = ({ filteredMates }) => {
 
-return (
-    <> 
-        {
-            filteredMates.map(mate => <CardHTML key={mate.id} mate={mate} className="col-md-6 col-lg-4 mb-5"/>)
-        }
-    </>
-)}
+    return (
+        <>
+            {
+                filteredMates.map(mate => <CardHTML key={mate.id} mate={mate} className="col-md-6 col-lg-4 mb-5" />)
+            }
+        </>
+    )
+}
